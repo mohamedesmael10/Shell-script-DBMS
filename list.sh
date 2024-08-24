b694533d73117a16f5c23c01ddb5e5d96987cba0
Mohamed Esmael <mohamed.mostafa.esmael10@outlook.com>
2024-08-24 14:22:34 +0300

    Add DBMS Selections

--- list.sh
+++ list.sh
@@ -21,8 +21,30 @@ while true; do
 ⠀⠀⠘⠻⡧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 EOF
 
-    tput setaf 6  
-    echo "$(tput setaf 6)Welcome to our Bash Shell Scripting Project$(tput setaf 2)"
-    echo "$(tput setaf 6)Implemented by: Mohamed Esmael & Hadeer Al-Bttal$(tput setaf 2)"
+    
+     echo "$(tput setaf 1)Welcome to our Bash Shell Scripting Project$(tput setaf 0)"
+     echo "$(tput setaf 1)Implemented by: Mohamed Esmael & Hadeer Al-Bttal$(tput setaf 0)"
+
+     tput setaf 2; 
+
+     echo "╔═══════════════════════════╗"
+     echo "║ 1 - Create Database       ║"
+     echo "║ 2 - List Databases        ║"
+     echo "║ 3 - Connect Database      ║"
+     echo "║ 4 - Drop Database         ║"
+     echo "║ 5 - Exit                  ║"
+     echo "╚═══════════════════════════╝"
+
+
+     tput setaf 7; 
+     echo -n "$(tput setaf 5)Select : $(tput setaf 7)"
+     read Select
+     case $Select in
+        1) . ./reate_db.sh ;;
+        2) ls Database 2>> error.log || echo "No Databases Found" ;;
+        3) . ./connect_db.sh ;;
+        4) . ./Drop_Database.sh ;;
+        5) exit ;;
+        *) echo -e "\n Invalid Selection (-_-;)・・・ \n" ;;
+     esac 
 done
- 
