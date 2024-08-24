0c18ecb9a3c33a6ee9f8725469f8fce1659c9f24
Mohamed Esmael <mohamed.mostafa.esmael10@outlook.com>
2024-08-24 15:26:27 +0300

    fix some issue at list.sh

--- list.sh
+++ list.sh
@@ -38,13 +38,23 @@ EOF
 
      tput setaf 7; 
      echo -n "$(tput setaf 5)Select : $(tput setaf 7)"
-     read Select
+     read -p "Enter your Selection (1-5): " Select
      case $Select in
-        1) . ./Create_Database.sh ;;
-        2) ls Database 2>> error.log || echo "No Databases Found" ;;
-        3) . ./Connect_Database.sh ;;
-        4) . ./Drop_Database.sh ;;
-        5) exit ;;
+        1) 
+			echo "You selected Create Database "
+			. ./Create_Database.sh ;;
+        2) 
+			echo "You selected List Databases "
+			ls Database 2>> error.log || echo "No Databases Found" ;;
+        3) 
+			echo "You selected Connect Database "
+			. ./Connect_Database.sh ;;
+        4) 
+			echo "You selected Drop Database "
+			. ./Drop_Database.sh ;;
+        5) 
+			echo "You selected Exit "
+			exit ;;
         *) echo -e "\n Invalid Selection (-_-;)・・・ \n" ;;
      esac 
 done
