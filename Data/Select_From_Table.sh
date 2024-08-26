ad014ec98ff6d51e675b328e099ae67c2b1a596a
Mohamed Esmael <mohamed.mostafa.esmael10@outlook.com>
2024-08-26 14:23:22 +0300

    fixed some issues in Data/Select_From_Table.sh

--- Data/Select_From_Table.sh
+++ Data/Select_From_Table.sh
@@ -12,21 +12,21 @@ function Database_connected() {
 function execute_sql() {
     sql_line=${entry//;/ }
     table_name=$(echo "$sql_line" | awk '{print $3}')
-    if [[ ! -f "$table_name" ]]; then echo "Error : Invalid Table Name" ; return; fi
+    if [[ ! -f "$table_name" ]]; then echo "Error : Invalid Table Name or Invalid Selection (-_-;)・・・ " ; return; fi
 
     select_column=$(echo "$sql_line" | awk '{print $1}')
     select_column_field=$(awk -F'|' 'BEGIN{found=0} {if(NR==1){for(i=1;i<=NF;i++){if($i=="'$select_column'")found=i}}} END{print found}' "$table_name")
-    if [[ $select_column_field == 0 ]] && [[ $select_column != "*" ]]; then echo "Error : Invalid Selected Column Name" && return; fi
+    if [[ $select_column_field == 0 ]] && [[ $select_column != "*" ]]; then echo "Error : Invalid Selected Column Name (-_-;)・・・" && return; fi
 
     if [[ "$sql_line" =~ WHERE ]]; then
         where_column=$(echo "$sql_line" | awk '{print $5}')
         where_column_field=$(awk -F'|' 'BEGIN{found=0} {if(NR==1){for(i=1;i<=NF;i++){if($i=="'$where_column'")found=i}}} END{print found}' "$table_name")
-        if [[ $where_column_field == 0 ]]; then echo "Error : Invalid Where Column Name"; return; fi
+        if [[ $where_column_field == 0 ]]; then echo "Error : Invalid Where Column Name (-_-;)・・・"; return; fi
 
         where_operator=$(echo "$sql_line" | awk '{print $4}')
         where_value=$(echo "$sql_line" | awk '{print $6}')
         where_value_exist=$(awk -F'|' 'BEGIN{found=0} {if(NR!=1){if($"'$where_column_field'"=="'$where_value'")found=1}} END{print found}' "$table_name")
-        if [[ $where_value_exist == 0 ]]; then echo "Warning : The Where Value does not exist in the Table "; fi
+        if [[ $where_value_exist == 0 ]]; then echo "Warning : The Where Value does not exist in the Table (-_-;)・・・ "; fi
 
         if [[ $select_column == "*" ]]; then
             awk -v were_value="$where_value" -F'|' '{if(NR!=1){if($"'$where_column_field'" '$where_operator' were_value){print $0}}}' "$table_name"
@@ -50,16 +50,16 @@ function execute_sql() {
      	╔═══════════════════════════╗
     	║ $db_name Connected        ║
      	╚═══════════════════════════╝
-      ╔═══════════════════════════════════════════════════════════════════════════════╗
-      ║    e.g. SELECT *; FROM table_name;                                            ║
-      ║    SELECT column; FROM table_name;                                            ║
-      ║    SELECT column ; FROM table_name ; WHERE column[==,<,>,>=,<=]value ;        ║
-      ║    SELECT * ; FROM table_name ; WHERE column[==,<,>,>=,<=]value ;             ║
-      ║    SELECT column; FROM table_name;                                            ║
-      ║    SELECT *; FROM table_name;                                                 ║
-      ╚═══════════════════════════════════════════════════════════════════════════════╝
+        ╔══════════════════════════════════════════════════════════════════════════════╗
+        ║    e.g. SELECT *; FROM table_name;                                           ║
+        ║    SELECT column; FROM table_name;                                           ║
+        ║    SELECT column ; FROM table_name ; WHERE column[==,<,>,>=,<=]value ;       ║
+        ║    SELECT * ; FROM table_name ; WHERE column[==,<,>,>=,<=]value ;            ║
+        ║    SELECT column; FROM table_name;                                           ║
+        ║    SELECT *; FROM table_name;                                                ║
+        ╚══════════════════════════════════════════════════════════════════════════════╝
      	╔═══════════════════════════╗
-    	║ 1 - Back to DB Menu       ║
+    	║ 1 - Back to Database Menu ║
      	║ 2 - Back to Main Menu     ║
      	╚═══════════════════════════╝
 EOF
@@ -67,8 +67,13 @@ EOF
     tput setaf 5
     read -p "$(tput setaf 5)Enter SQL Statement : " Select
     case $Select in
-    1) exit ;;
-    2) exit 2 ;;
+    1) 
+    echo "You selected Back to Database Menu "
+    exit ;;
+    2) 
+    echo "You selected Back to Main Menu "
+    exit 2 ;;
     *) execute_sql ;;
+         
     esac
 done
