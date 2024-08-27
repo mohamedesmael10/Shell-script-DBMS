f1a31b5d209ff2444ce2d05a743e8cdbba6b874b
hadeer-tsu <hadeeralbttal@gmail.com>
2024-08-28 00:49:38 +0300

    add Drop_Database code

--- Data/Drop_Database.sh
+++ Data/Drop_Database.sh
@@ -1,1 +1,23 @@
-echo "$(tput setaf 1)Implemented by: Mohamed Esmael & Hadeer Al-Bttal$(tput setaf 0)"
+#!/bin/bash
+
+# Prompt the user to enter the database name
+read -p "Enter Database Name: " name
+
+# Check if the directory for the database exists
+if [ -d "DBMS/$name" ]; then
+    # Confirm if the user wants to delete the database
+    read -p "Are you sure you want to delete '$name'? (Y/N): " choice
+    case $choice in
+        [yY]|[yY][eE][sS])
+            rm -r "DBMS/$name" && echo "$name deleted successfully" || echo "Failed to delete $name"
+            ;;
+        [nN]|[nN][oO])
+            echo "Operation canceled"
+            ;;
+        *)
+            echo "Invalid option. Please enter Y or N."
+            ;;
+    esac
+else
+    echo "Database '$name' does not exist."
+fi
